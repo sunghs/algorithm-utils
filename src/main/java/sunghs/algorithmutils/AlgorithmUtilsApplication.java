1948f9db23aaa1846af87a68d4c866285aa1cb39
sunghs <sunghs9071@naver.com>
2021-03-23 09:15:55 +0900

    commit

---
+++ src/main/java/sunghs/algorithmutils/AlgorithmUtilsApplication.java
@@ -0,0 +1,13 @@
+package sunghs.algorithmutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/algorithmutils/AlgorithmUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.algorithmutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
