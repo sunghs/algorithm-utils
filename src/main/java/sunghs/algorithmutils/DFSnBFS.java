2af01daad3ae24889fe41e2393ee311af704605b
sunghs <sunghs9071@naver.com>
2021-03-24 09:42:13 +0900

    길찾기 bfs 추가

--- src/main/java/sunghs/algorithmutils/DFSnBFS.java
+++ src/main/java/sunghs/algorithmutils/DFSnBFS.java
@@ -1,12 +1,16 @@
 package sunghs.algorithmutils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import sunghs.algorithmutils.data.Coordinate;
+import sunghs.algorithmutils.data.TreeNode;
 
 @Slf4j
 public class DFSnBFS {
@@ -102,7 +106,7 @@ public class DFSnBFS {
     }
 
     /**
-     * recursive dfs 를 위한 호출,
+     * n_m_dfs 메소드의 recursive dfs 를 위한 호출,
      * 해당 좌표의 map을 1로 만들고 동서남북으로 recursive 해 1로 만들고 한덩어리 true 를 return 함
      *
      * @param map NxM 지도
@@ -125,10 +129,60 @@ public class DFSnBFS {
         return false;
     }
 
+    /**
+     * 미로찾기, 미로찾기/길찾기 문제는 bfs로 해결
+     * @param map 맵지도, 1이 길, 0이 벽
+     * @return 최단거리 이동횟수
+     */
+    public int miro_bfs(int[][] map) {
+        List<Integer> findList = new ArrayList<>();
+        Queue<Coordinate> queue = new LinkedList<>();
+        queue.add(new Coordinate(0,0, 1));
 
+        int arriveX = map.length - 1;
+        int arriveY = map[0].length - 1;
+
+        while (!queue.isEmpty()) {
+            Coordinate current = queue.poll();
+            map[current.x][current.y] = 2; // 1이 길이고, 0이 벽이므로, 2는 지나온 걸로 마킹
+
+            // 동서남북 조회해서 길이면 순회할 수 있도록 queue에 삽입
+            miro_core_bfs(map, current.x-1, current.y, current.moveCnt, queue);
+            miro_core_bfs(map, current.x+1, current.y, current.moveCnt, queue);
+            miro_core_bfs(map, current.x, current.y-1, current.moveCnt, queue);
+            miro_core_bfs(map, current.x, current.y+1, current.moveCnt, queue);
+
+            // 맵의 마지막 좌표 (도착지점)에 먼저 들어오면 찾은 길 리스트에 넣어줌
+            if(current.x == arriveX && current.y == arriveY) {
+                findList.add(current.moveCnt);
+            }
+        }
+        Collections.sort(findList);
+        // 정렬 후 가장 적은 moveCnt 는 최단거리, findList.size 는 찾은 길의 리스트
+        log.info("Road Count : {}", findList.size());
+        return findList.size() > 0 ? findList.get(0) : -1;
+    }
+
+    /**
+     * recursive 할 메소드, 특정 좌표가 길인지 여부를 판단해서 queue에 삽입
+     * @param map 마킹할 지도
+     * @param x 움직일 좌표 x
+     * @param y 움직일 좌표 y
+     * @param moveCnt 직전 움직인 횟수, 길을 찾았다면 직전 움직인 횟수 + 1을 해서 큐에 넣는다.
+     * @param queue 순회 할 큐
+     */
+    public void miro_core_bfs(int[][] map, int x, int y, int moveCnt, Queue<Coordinate> queue) {
+        if(x < 0 || x >= map.length || y < 0 || y >= map[0].length) {
+            return;
+        }
+        if(map[x][y] == 1) {
+            queue.add(new Coordinate(x, y, ++moveCnt));
+        }
+    }
 
 
     @Test
+    @DisplayName("TreeNode dfs, bfs 순회")
     public void test() {
         TreeNode treeNode = get();
 
@@ -148,6 +202,7 @@ public class DFSnBFS {
     }
 
     @Test
+    @DisplayName("NxM 맵 경우의 수 찾기 (음료수 얼려먹기)")
     public void test2() {
         int[][] map = {
             {0, 0, 1, 1, 0},
@@ -156,6 +211,21 @@ public class DFSnBFS {
             {0, 0, 0, 0, 0}
         };
         int result = n_m_dfs(map);
-        log.info("result {}", result);
+        log.info("result : {}", result);
+    }
+
+    @Test
+    @DisplayName("미로 길찾기, 1이 길 0이 벽")
+    public void test3() {
+        int[][] map = {
+            {1, 1, 1, 1, 0},
+            {0, 1, 0, 1, 1},
+            {1, 1, 0, 1, 1},
+            {0, 1, 0, 0, 1},
+            {0, 1, 1, 0, 1},
+            {0, 0, 1, 1, 1}
+        };
+        int result = miro_bfs(map);
+        log.info("result : {}", result);
     }
 }
---
+++ src/main/java/sunghs/algorithmutils/data/Coordinate.java
@@ -0,0 +1,14 @@
+package sunghs.algorithmutils.data;
+
+public class Coordinate {
+
+    public int x;
+    public int y;
+    public int moveCnt;
+
+    public Coordinate(int x, int y, int moveCnt) {
+        this.x = x;
+        this.y = y;
+        this.moveCnt = moveCnt;
+    }
+}
