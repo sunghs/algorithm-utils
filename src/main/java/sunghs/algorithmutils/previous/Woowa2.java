63c5abd6e0fac39e82b75b6867de337aef389d94
sunghs <sunghs9071@naver.com>
2021-03-28 23:31:08 +0900

    컨벤션 정렬

--- src/main/java/sunghs/algorithmutils/previous/Woowa2.java
+++ src/main/java/sunghs/algorithmutils/previous/Woowa2.java
@@ -3,10 +3,10 @@ package sunghs.algorithmutils.previous;
 /**
  * 어느 연못에 엄마 말씀을 좀처럼 듣지 않는 청개구리가 살고 있었습니다.
  * 청개구리는 엄마가 하는 말은 무엇이든 반대로 말하였습니다.
- *
+ * <p>
  * 엄마 말씀 word가 매개변수로 주어질 때, 아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록
  * solution 메서드를 완성해주세요.
- *
+ * <p>
  * A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z
  * Z	Y	X	W	V	U	T	S	R	Q	P	O	N	M	L	K	J	I	H	G	F	E	D	C	B	A
  * 제한사항
@@ -18,7 +18,7 @@ package sunghs.algorithmutils.previous;
  * I love you	R olev blf
  * 입출력 예 설명
  * 입출력 예 #1
- *
+ * <p>
  * 'I'는 알파벳 대문자이므로 'R'로 변환할 수 있습니다.
  * 공백(스페이스 바)은 알파벳 외의 문자이므로 변환하지 않습니다.
  * 'l'은 알파벳 소문자이므로 'o'로 변환할 수 있습니다.
@@ -46,12 +46,12 @@ public class Woowa2 {
         // 219 - 원래소문자 = 반전소문자
 
         String result = "";
-        for(int i = 0; i < word.length(); i ++) {
+        for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if(c >= 'A' && c <= 'Z') {
-                result += (char)(155 - c);
-            } else if(c >= 'a' && c <= 'z') {
-                result += (char)(219 - c);
+            if (c >= 'A' && c <= 'Z') {
+                result += (char) (155 - c);
+            } else if (c >= 'a' && c <= 'z') {
+                result += (char) (219 - c);
             } else {
                 // 대소문자가 아니면 그냥 더함
                 result += String.valueOf(c);
